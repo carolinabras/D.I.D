019d0b9df1568594060e8fcf5ec45572f5280e76
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2023-05-03 00:14:15 +0100

    task class (not yet totally implemented!!)

---
+++ Assets/Scripts/TaskIGNORE.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskIGNORE : MonoBehaviour
+{
+
+    public string taskDescription;
+    public int taskID;
+    public bool isCompleted;
+
+    public string proficiency; // the proficiency that this task is associated with
+    
+    public TaskIGNORE(string description, int id)
+    {
+        taskDescription = description;
+        taskID = id;
+        isCompleted = false;
+    }
+
+    public void CompleteTask()
+    {
+        isCompleted = true;
+    }
+    
+}
