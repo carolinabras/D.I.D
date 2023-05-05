68367e08172da2e52bb2b91c2771b602b1f9ef34
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2023-05-05 23:51:13 +0100

    shadow working

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -9,15 +9,146 @@ public class TaskManager : MonoBehaviour
     [SerializeField] 
     private TMPro.TMP_Text taskText;
 
+    public Sprite shadowSprite;
+
+  
+
+
     public Task currentTask;
 
+    public bool shadowActive = false;
+
     public AudioSource audioSource;
+
+
+public GameObject dogButton;
+public GameObject dogShadowButton;
+
+public GameObject childButton;
+
+public GameObject childShadowButton;
+
+public GameObject elderButton;
+
+public GameObject elderShadowButton;
+
+public GameObject maskButton;
+
+public GameObject maskShadowButton;
+
+
+
     public void playSoundEffect() {
         audioSource.Play();
     }
      public void Start()
     {
 
+        int randomNumber = Random.Range(0, 7);
+        if (randomNumber > 5) {
+            shadowActive = true;
+        }
+        else {
+            shadowActive = false;
+        }
+
+        //if shadow active then pick two random buttons from the four: "Dog", "Child", "Elder", "Mask" and make them inactive and change their sprite to the shadow sprite
+        if (shadowActive) {
+            
+
+            GameObject dogButton = this.dogButton;
+            GameObject childButton = GameObject.Find("Child");
+            GameObject elderButton = GameObject.Find("Elder");
+            GameObject maskButton = GameObject.Find("Mask");
+
+            GameObject[] buttons = {dogButton, childButton, elderButton, maskButton};
+            int randomButton1 = Random.Range(0, 4);
+            int randomButton2 = Random.Range(0, 4);
+
+            while (randomButton1 == randomButton2) {
+                randomButton2 = Random.Range(0, 4);
+            }
+
+            if (buttons[randomButton1] == dogButton) {
+                dogButton.SetActive(false);
+                dogShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton1].SetActive(false);
+            }
+
+             if (buttons[randomButton1] == childButton) {
+                childButton.SetActive(false);
+                childShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton1].SetActive(false);
+            }
+
+             if (buttons[randomButton1] == elderButton) {
+                elderButton.SetActive(false);
+                elderShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton1].SetActive(false);
+            }
+
+            if (buttons[randomButton1] == maskButton) {
+                maskButton.SetActive(false);
+                maskShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton1].SetActive(false);
+            }
+
+            if (buttons[randomButton2] == dogButton) {
+                dogButton.SetActive(false);
+                dogShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton2].SetActive(false);
+            }
+
+             if (buttons[randomButton2] == childButton) {
+                childButton.SetActive(false);
+                childShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton2].SetActive(false);
+            }
+
+             if (buttons[randomButton2] == elderButton) {
+                elderButton.SetActive(false);
+                elderShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton2].SetActive(false);
+            }
+
+            if (buttons[randomButton2] == maskButton) {
+                maskButton.SetActive(false);
+                maskShadowButton.SetActive(true);
+            }
+            else {
+                buttons[randomButton2].SetActive(false);
+            }
+
+            // Button button1 = buttons[randomButton1].GetComponent<Button>();
+            // if (button1 == null) {
+            //     Debug.Log("Button not found");
+            //     return;
+            // }
+            // Button button2 = buttons[randomButton2].GetComponent<Button>();
+            // if (button2 == null) {
+            //     Debug.Log("Button not found");
+            //     return;
+            // }
+
+            // button1.interactable = false;
+            // button2.interactable = false;
+            // button1.image.sprite = shadowSprite;
+            // button2.image.sprite = shadowSprite;
+        }
 
         if ( taskText == null ) {
             Debug.Log("Task text not found");
@@ -109,4 +240,5 @@ public class TaskManager : MonoBehaviour
         }
     }
 
-}
+    }
+
